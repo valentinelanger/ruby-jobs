daba26f749654c8382a1605ee753369a667579aa
Valentine Langer <valentine.langer@gmail.com>
2018-02-04 22:56:28 +0100

    level1

--- level1/main.rb
+++ level1/main.rb
@@ -1,3 +1,36 @@
 require "json"
 
-# your code
+data = JSON.parse(File.read('data.json'))
+workers = data['workers']
+shifts = data['shifts']
+
+def count_shift_per_worker(shifts, worker_id)
+    shifts.count do |shift|
+        shift['user_id'] == worker_id
+    end
+end
+
+def calculate_wage_per_worker(nb_shifts, worker)
+    nb_shifts * worker['price_per_shift']
+end
+
+def calculate_wages(workers, shifts)
+    workers.each do |worker|
+        worker['price'] = calculate_wage_per_worker(count_shift_per_worker(shifts, worker['id']), worker)
+    end
+end
+
+def build_worker_list(workers)
+    workers.each do |worker|
+        worker.select! { |k,v| k == 'id' || k == 'price' }
+    end
+    return { 'workers' => workers }
+end
+
+def store_price_per_user(workers)
+    File.open('output_vl.json', 'wb') do |file|
+      file.write(JSON.pretty_generate(workers))
+    end
+end
+
+store_price_per_user(build_worker_list(calculate_wages(workers, shifts)))
